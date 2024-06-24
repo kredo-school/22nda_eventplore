9bb15b64df09f742c7fc258a207176d42462698c
Ota Marika <mr09_gypsophila@icloud.com>
2024-06-24 21:08:59 +0900

    Revised regarding of Bootstrap link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,9 +24,6 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-    {{-- Bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></head>
-<body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
