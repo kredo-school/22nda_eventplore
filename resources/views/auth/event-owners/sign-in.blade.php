2745ba4f0ad4532d74a6c3d0af8f7a8efe44898b
TakeshitaHaruka <detstably.abyss@gmail.com>
2024-06-24 20:27:30 +0900

    Fix design of sign-in(owner) and change style.css

--- resources/views/auth/event-owners/sign-in.blade.php
+++ resources/views/auth/event-owners/sign-in.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-5">
+<div class="container-fluid p-0" style="width: 100vw; height: 100vh;">
+    <div class="d-flex justify-content-center w-100 h-100">
+        <div class="w-50 p-0">
             <form method="POST" action="">
                 @csrf
 
                 <div class="row justify-content-center my-5 text-center">
-                    <img src="{{ asset('images/eventplore-logo_final-nobg_480.png') }}" alt="logo" class="mt-5 mb-4 w-25">
+                    <img src="{{ asset('images/eventplore-logo_final-nobg_480.png') }}" alt="logo" class="mt-5 mb-2 w-25">
                     <h2 class="h1 mb-2 text-align-center">Event Owner</h2>
                     <div class="row justify-content-center mb-3">
-                        <div class="col-12"><p>Don't you have account yet?&nbsp;<span class="fw-bold" style="color: #84947C">Sign Up</span></p></div>
+                        <div class="col-12"><p>Don't you have account yet?&nbsp;<a href="#" class="fw-bold text-decoration-none" style="color: #84947C">Sign Up</a></p></div>
                     </div>
 
                     <div class="row justify-content-center mb-3">
                         <div class="col-md-4 text-start">
                             <label for="username" class="form-label fw-bold mb-2">Username*</label>
                             <div class="col-auto">
-                                <input id="username" type="username" class="form-control" name="username" required autocomplete="username" autofocus placeholder="Username">
+                                <input id="username" type="username" class="form-control" name="username" required autocomplete="username" autofocus placeholder="Username" style="border: 1px solid #84947C">
         
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
@@ -33,13 +33,11 @@
                         <div class="col-md-4 text-start">
                             <label for="password" class="form-label fw-bold mb-2">Password*</label>
                             <div class="col-auto">
-                                {{-- <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" placeholder="Password"><i class="fa-solid fa-eye-slash text-end"></i> --}}
-                                <div class="input-group mb-3">
-                                    {{-- <input type="text" class="form-control" placeholder="Password" aria-label="Recipient's username" aria-describedby="button-addon2">
-                                    <i class="fa-solid fa-eye-slash text-end rounded border-start-none" style="padding-top: 10px; padding-left: 10px; padding-right: 10px; border: 1px solid #84947C"></i> --}}
-                                    <input type="password" name="" id="password" reequired="">
-                                    <label for="Password"></label>
-                                    <span class="password-toggle-"><i class="fa-solid fa-eye-slash text-end"></i></span>
+                                <div class="input-group mb-3 position-relative">
+                                    <input type="text" class="form-control rounded" style="border: 1px solid #84947C" placeholder="Password">
+                                    <div class="d-flex h-100 end-0 p-2 position-absolute justify-content-center align-items-center">
+                                        <i class="fa-solid fa-eye-slash"></i>
+                                    </div>
                                 </div>
         
                                 @error('password')
@@ -80,8 +78,8 @@
                 </div>
             </form>
         </div>
-        <div class="col-7">
-            <img src="{{ asset('images/sign-in/product-school-ITF7fD0TSCY-unsplash.jpg') }}" alt="register event owner photo" class=" img-fluid h-100" style="margin-top: 50px; padding-right:50px">
+        <div class="w-50 p-0">
+            <img src="{{ asset('images/sign-in/product-school-ITF7fD0TSCY-unsplash.jpg') }}" alt="register event owner photo" class=" img-fluid h-100">
         </div>
     </div>
 </div>
