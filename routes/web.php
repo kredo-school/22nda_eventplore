58530e3a40385fab2170c05f8d2e9d99102cb691
Ota Marika <mr09_gypsophila@icloud.com>
2024-06-26 21:17:59 +0900

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,11 @@ Route::get('/auth/event-owners/sign-up', function () {
     return view('auth.event-owners.sign-up');
 });
 
+
+//Marika
+Route::get('/event-owners/events/show', function () {
+    return view('event-owners.events.show');
+});
+
+
 ?>
