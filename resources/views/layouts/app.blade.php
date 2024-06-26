8efb83da7a3b16372bc083043390c387a0f77948
LAPTOP-Q0BDBSNE\kana <kanako10minami@gmail.com>
2024-06-26 20:56:23 +0900

    Merge branch 'main' of https://github.com/kredo-school/22nda_eventplore
    # Conflicts:
    #	package-lock.json

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,6 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-
 </head>
 <body>
     <div id="app">
--- resources/views/users/profile/show.blade.php
+++ resources/views/users/profile/show.blade.php
@@ -1,7 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
+
+<link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+
+<div class="profile-container">
+    <div class="background-image" style="background-image: url('{{ asset('images/profile/profileback.jpg') }}');"></div>
+    <div class="content">
+        <div class="card mx-auto">
+            <div class="card-body text-center">
+                <i class="fa-solid fa-circle-user"></i>
+                <h3 class="card-title">John</h3>
+                <p class="text-muted">John Smith</p>
+                <div class="row mt-4">
+                    <div class="col-6">
+                        <div class="card">
+                            <div class="card-body">
+                            <h5 class="card-title">24</h5>
+                            <p class="card-text">Events</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card">
+                            <div class="card-body">
+                            <h5 class="card-title">5</h5>
+                            <p class="card-text">Comments</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- <div>
     <img src="{{ asset('images/profile/profileback.jpg') }}" class="object-fit" alt="Profile Picture">
     <div class="card mx-auto" style="max-width: 400px;">
         <div class="card-body text-center">
@@ -27,6 +60,6 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 
 @endsection
