1982c53018413b133c80d662cbec269ae99dcd1d
TakeshitaHaruka <detstably.abyss@gmail.com>
2024-07-03 20:01:03 +0900

    Make hamburger-menu of navbar

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,17 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 // Haruka
-// Show sign-in page for event-owner
+// Show sign-up page for event-owner
 Route::get('/auth/event-owners/sign-up', function () {
     return view('auth.event-owners.sign-up');
 });
-
+// Show sign-in page for event-owner
 Route::get('/auth/event-owners/sign-in', function () {
     return view('auth.event-owners.sign-in');
 });
 // Show sign-in page for user
 Route::get('/auth/users/sign-in', function () {
     return view('auth.users.sign-in');
-});
-// Show sign-up page for event-owner
-Route::get('/auth/users/sign-up', function () {
-    return view('auth.users.sign-up');
 });
 // Show sign-up page for user
 Route::get('/auth/users/sign-up', function () {
