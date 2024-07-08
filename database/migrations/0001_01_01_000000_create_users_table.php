c80f3594b323a7327617fc3a68eb81cdb51ed085
Naoki Tonegawa <naoki.tonegawa25@gmail.com>
2024-07-08 21:24:57 +0900

    Create migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username');
+            $table->string('email')->unique();
             $table->longText('avatar')->nullable();
             $table->string('first_name');
             $table->string('last_name');
