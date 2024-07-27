39a03ea6fd5e04151d9ba4779b7e7cba8c65204f
LAPTOP-Q0BDBSNE\kana <kanako10minami@gmail.com>
2024-07-27 13:50:30 +0900

    add reviewcontroller

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ReviewController extends Controller
+{
+    public function store(Request $request, $eventId)
+    {
+        $request->validate([
+            'star' => 'required|integer|min:1|max:5',
+            'comment' => 'nullable|string',
+        ]);
+
+        $event = Event::with('reviews.user')->findOrFail($eventId);
+
+        Review::create([
+            'event_id' => $event->id,
+            'user_id' => Auth::id(),
+            'star' => $request->star,
+            'comment' => $request->comment,
+        ]);
+
+        return redirect()->back();
+    }
+}
