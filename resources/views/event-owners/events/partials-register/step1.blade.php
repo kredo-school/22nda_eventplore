9cc3ad1a243fe0007cc68d6720b790946cdd2893
TakeshitaHaruka <detstably.abyss@gmail.com>
2024-08-15 21:40:39 +0900

    Fix validation of category

--- resources/views/event-owners/events/partials-register/step1.blade.php
+++ resources/views/event-owners/events/partials-register/step1.blade.php
@@ -1,7 +1,7 @@
 <div class="row justify-content-center mx-5 px-5">
     <label for="event_name" class="fw-bold mb-2 text-start">Event Name*</label>
     <div class="col-12 mb-3">
-        <input id="event_name" type="text" class="form-control @error('event_name') is-invalid @enderror" name="event_name" required autocomplete="off" autofocus placeholder="Event Name" style="border: 1px solid #84947C">
+        <input id="event_name" type="text" class="form-control @error('event_name') is-invalid @enderror" name="event_name" required autocomplete="off" autofocus value="{{ old('event_name') }}" placeholder="Event Name" style="border: 1px solid #84947C">
         @error('event_name')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -13,7 +13,7 @@
 <div class="row justify-content-center mx-5 px-5">
     <div class="col-lg-6 mb-3 text-start">
         <label for="start_date" class="fw-bold mb-2 text-start">Start Date*</label>
-        <input type="date" name="start_date" id="start_date" class="form-control @error('start_date') is-invalid @enderror" required autocomplete="start_date" autofocus style="border: 1px solid #84947C">
+        <input type="date" name="start_date" id="start_date" class="form-control @error('start_date') is-invalid @enderror" required autocomplete="start_date" autofocus value="{{ old('start_date') }}" style="border: 1px solid #84947C">
         @error('start_date')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -22,7 +22,7 @@
     </div>
     <div class="col-lg-6 mb-3 text-start">
         <label for="finish_date" class="fw-bold mb-2 text-start">Finish Date*</label>
-        <input type="date" name="finish_date" id="finish_date" class="form-control @error('finish_date') is-invalid @enderror" required autocomplete="finish_date" autofocus style="border: 1px solid #84947C">
+        <input type="date" name="finish_date" id="finish_date" class="form-control @error('finish_date') is-invalid @enderror" required autocomplete="finish_date" autofocus value="{{ old('finish_date') }}" style="border: 1px solid #84947C">
         @error('finish_date')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -31,7 +31,7 @@
     </div>
     <div class="col-lg-6 mb-3 text-start">
         <label for="start_time" class="fw-bold mb-2 text-start">Start Time*</label>
-        <input type="time" name="start_time" step="1800" value="00:00" id="start_time" class="form-control @error('start_time') is-invalid @enderror" required autocomplete="start_time" autofocus style="border: 1px solid #84947C">
+        <input type="time" name="start_time" step="1800" value="00:00" id="start_time" class="form-control @error('start_time') is-invalid @enderror" required autocomplete="start_time" autofocus value="{{ old('start_time') }}" style="border: 1px solid #84947C">
         @error('start_time')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -40,7 +40,7 @@
     </div>
     <div class="col-lg-6 mb-3 text-start">
         <label for="finish_time" class="fw-bold mb-2 text-start">Finish Time*</label>
-        <input type="time" name="finish_time" step="1800" value="00:00" id="finish_time" class="form-control @error('finish_time') is-invalid @enderror" required autocomplete="finish_time" autofocus style="border: 1px solid #84947C">
+        <input type="time" name="finish_time" step="1800" value="00:00" id="finish_time" class="form-control @error('finish_time') is-invalid @enderror" required autocomplete="finish_time" autofocus value="{{ old('finish_time') }}" style="border: 1px solid #84947C">
         @error('finish_time')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -52,7 +52,7 @@
 <div class="row justify-content-center mx-5 px-5">
     <label for="details" class="fw-bold mb-2 text-start">Content*</label>
     <div class="col-12 mb-3">
-        <textarea name="details" id="details" rows="3" class="form-control @error('details') is-invalid @enderror" required placeholder="Content" style="border: 1px solid #84947C"></textarea>
+        <textarea name="details" id="details" rows="3" class="form-control @error('details') is-invalid @enderror" required placeholder="Content" style="border: 1px solid #84947C">{{ old('details') }}</textarea>
         @error('details')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -64,7 +64,7 @@
 <div class="row justify-content-center mx-5 px-5">
     <label for="history" class="fw-bold mb-2 text-start">History*</label>
     <div class="col-12 mb-3">
-        <textarea name="history" id="history" rows="3" class="form-control @error('history') is-invalid @enderror" required placeholder="History" style="border: 1px solid #84947C"></textarea>
+        <textarea name="history" id="history" rows="3" class="form-control @error('history') is-invalid @enderror" required placeholder="History" style="border: 1px solid #84947C">{{ old('history') }}</textarea>
         @error('history')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
--- resources/views/event-owners/events/partials-register/step6.blade.php
+++ resources/views/event-owners/events/partials-register/step6.blade.php
@@ -6,7 +6,7 @@
         <i class="fa-brands fa-product-hunt icon-lg"></i>
     </div>
     <div class="col-9">
-        <input id="parking" type="text" class="form-control" name="parking" required autocomplete="parking" autofocus placeholder="Add parking information" style="border: 1px solid #84947C">
+        <input id="parking" type="text" class="form-control" name="parking" required autocomplete="parking" autofocus value="{{ old('parking') }}" placeholder="Add parking information" style="border: 1px solid #84947C">
         @error('parking')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -20,7 +20,7 @@
         <i class="fa-solid fa-train-subway icon-lg"></i>
     </div>
     <div class="col-9">
-        <input id="train" type="text" class="form-control" name="train" required autocomplete="train" autofocus placeholder="Add train/bus information" style="border: 1px solid #84947C">
+        <input id="train" type="text" class="form-control" name="train" required autocomplete="train" autofocus value="{{ old('train') }}" placeholder="Add train/bus information" style="border: 1px solid #84947C">
         @error('train')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -34,7 +34,7 @@
         <i class="fa-solid fa-restroom icon-lg"></i>
     </div>
     <div class="col-9">
-        <input id="toilet" type="text" class="form-control" name="toilet" required autocomplete="toilet" autofocus placeholder="Add toilet information" style="border: 1px solid #84947C">
+        <input id="toilet" type="text" class="form-control" name="toilet" required autocomplete="toilet" autofocus value="{{ old('toilet') }}" placeholder="Add toilet information" style="border: 1px solid #84947C">
         @error('toilet')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -48,7 +48,7 @@
         <i class="fa-solid fa-cloud-showers-heavy icon-lg"></i>
     </div>
     <div class="col-9">
-        <input id="weather" type="text" class="form-control" name="weather" required autocomplete="weather" autofocus placeholder="Add weather information" style="border: 1px solid #84947C">
+        <input id="weather" type="text" class="form-control" name="weather" required autocomplete="weather" autofocus value="{{ old('weather') }}" placeholder="Add weather information" style="border: 1px solid #84947C">
         @error('weather')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -60,14 +60,14 @@
 <div class="row justify-content-center mx-5 px-5 mb-4">
     <div class="col-3">
         <select name="category" id="category" class="form-select me-2 required">
-            <option value="" hidden>Category</option>
+            <option value="{{ old('category_id') }}" hidden>Category</option>
             @foreach ($categories as $index => $category)
             <option value="{{ $category->id }}">{{ $category->name }}</option>
         @endforeach
         </select>
     </div>
     <div class="col-9">
-        <input id="add_info" type="text" class="form-control" name="add_info" required autocomplete="add_info" autofocus placeholder="Add more information" style="border: 1px solid #84947C">
+        <input id="add_info" type="text" class="form-control" name="add_info" required autocomplete="add_info" autofocus value="{{ old('add_info') }}" placeholder="Add more information" style="border: 1px solid #84947C">
         @error('add_info')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
